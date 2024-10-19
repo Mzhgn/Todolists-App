3a69df08d286871018fb1c14704234ddfc2afa96
Mozhgan <mojganshah@gmail.com>
2024-10-19 22:43:46 +0330

    fontawsome installed

--- src/components/TodoList/Todo.js
+++ src/components/TodoList/Todo.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default class Todo extends Component {
+  render() {
+    return (
+      // 'completed' class for completed todos
+      <div className="todo" style={{ display: "flex" }}>
+        <li className="todo-item">Title</li>
 
-    render() {
-        return (
-            // 'completed' class for completed todos
-            <div className='todo' style={{ display: 'flex' }}>
-                <li className="todo-item">{this.props.title}</li>
+        <button className="check-btn">
+          <i className="fas fa-check" aria-hidden="true"></i>
+        </button>
 
-                <button className="check-btn">
-                    <i className="fas fa-check" aria-hidden="true"></i>
-                </button>
-
-                <button className="trash-btn">
-                    <i className="fas fa-trash" aria-hidden="true"></i>
-                </button>
-            </div>
-        )
-    }
+        <button className="trash-btn">
+          <i className="fas fa-trash" aria-hidden="true"></i>
+        </button>
+      </div>
+    );
+  }
 }
