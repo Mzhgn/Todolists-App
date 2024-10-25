bfedaf09271f5b2a896144a7ac37eed5ac002eb4
Mozhgan <mojganshah@gmail.com>
2024-10-25 22:28:50 +0330

    refactor the codes based on functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react'
-import TodoList from './components/TodoList/TodoList'
-import './App.css'
+import React, { Component } from "react";
+import TodoList from "./components/TodoList/TodoList";
+import "./App.css";
 
-export default class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <TodoList></TodoList>
-            </div>
-        )
-    }
+export default function App() {
+  return (
+    <div>
+      <TodoList></TodoList>
+    </div>
+  );
 }
